fd05f6c9b7859a5a3ae729153c843b0662557dc2
Nicholas Sacayan (Atom) <nksacayan@gmail.com>
2021-08-12 22:12:55 -0700

    Adds vanilla bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import Titlebar from "./components/Titlebar";
 import Navbar from "./components/Navbar";
 import MainContent from "./components/MainContent";
 
 function App() {
   return (
     <>
-      <Titlebar />
       <Navbar />
       <MainContent />
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 function Navbar() {
   return (
     <nav>
+      <h1>Title Here</h1>
       <ul>
         <li>
           <a href="https://reactjs.org/docs/hello-world.html">Item 1</a>

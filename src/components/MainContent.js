7a4c71c75a6a71cf5f2f73fb4a77736d0fabe2e2
Nicholas Sacayan (Atom) <nksacayan@gmail.com>
2021-08-18 13:11:58 -0700

    Establishes template

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,11 +1,21 @@
-import Container from "react-bootstrap/Container";
-import AnimeCarousel from "./AnimeCarousel";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+import flowers from "../images/flowers.jpg";
 
 function MainContent() {
   return (
-    <Container>
-      <AnimeCarousel />
-    </Container>
+    <>
+      <Card className="bg-dark text-white">
+        <Card.Img src={flowers} alt="Card image" />
+        <Card.ImgOverlay>
+          <Card.Title>Anime Title</Card.Title>
+          <Card.Text>This is where the anime summary goes.</Card.Text>
+          <Card.Text>Genres...</Card.Text>
+        </Card.ImgOverlay>
+        <Button variant="primary">Randomize. Make this smaller lmao</Button>
+      </Card>
+    </>
   );
 }
 

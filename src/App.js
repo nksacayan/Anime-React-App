7d1383eee12b9890856b4ee5c247b8b2ec47a82b
Nicholas Sacayan (Atom) <nksacayan@gmail.com>
2021-08-20 15:14:41 -0700

    Moves ApolloProvider to App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import "./App.css";
 import MyNavbar from "./components/Navbar";
 import MainContent from "./components/MainContent";
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  // useQuery,
+  // gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://graphql.anilist.co/",
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <MyNavbar />
       <MainContent />
-    </>
+    </ApolloProvider>
   );
 }
 
